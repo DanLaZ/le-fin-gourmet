16df94090566696696c77d3a48c33c27ce571dfa
DanLaZ <lazenberry_daniel@yahoo.com>
2019-11-14 23:03:39 -0500

    Routes are half way written out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+// import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-
+      
+      
     </div>
+
+    // Use once components are built out for the first page
+    // <Router>
+    //   <Route exact path="/home" component={homePage} />
+    //   <Route exact path="/about" component={aboutPage} />
+    //   <Route exact path="/menu/" component={menu} />
+    //   <Route exact path="/test" component={test} />
+    // </Router>
   );
 }
 
