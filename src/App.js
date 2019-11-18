f42e53d1073033685953f4e96106274892ee1228
DanLaZ <lazenberry_daniel@yahoo.com>
2019-11-18 12:25:34 -0500

    Private dining pages routes are written out

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ function App() {
     // Use once components are built out for the first page
     <Router>
       {/*Home Page*/}
-      <Route exact path="/home" component={homePage} />
+      <Route exact path="/home" component={home} />
 
       {/*About Pages*/}
-      <Route exact path="/about" component={aboutResturant} />
-      <Route exact path="/about/maguy-le-coze" component={aboutOwnerOne} />
-      <Route exact path="/about/eric-ripert" component={aboutOwnerTwo} />
+      <Route exact path="/about" component={about_resturant} />
+      <Route exact path="/about/maguy-le-coze" component={about_owner1} />
+      <Route exact path="/about/eric-ripert" component={about_owner2} />
 
       {/*Menu Pages*/}
       <Route exact path="/menus/dining-room/lunch" component={lunch} />
@@ -28,14 +28,22 @@ function App() {
       <Route exact path="/menus/dining-room/chefs-tasting" component={chefs_tasting} />
       <Route exact path="/menus/dining-room/le-fin-tasting" component={le_fin_tasting} />
       <Route exact path="/menus/dining-room/vegetarian-tasting" component={vegetarian_tasting } />
+      <Route exact path="/menus/lounge/lounge-menu" component={lounge_menu} />
+      <Route exact path="/menus/lounge/city-harvest" component={city_harvest} />
+      <Route exact path="/menus/lounge/cocktails" component={cocktails} />
+      <Route exact path="/menus/lounge/wine-and-spirits" component={wine_and_spirits} />
+      <Route exact path="/menus/wine/wine-program" component={wine_program} />
       
       {/*Private Dining Pages*/}
-      <Route exact path="/menus/lounge/lounge-menu" component={lounge_menu} />
-      <Route exact path="/menus/lounge/city-harvest" component={lounge_menu} />
-      <Route exact path="/menus/lounge/cocktails" component={lounge_menu} />
-      <Route exact path="/menus/lounge/wine-and-spirits" component={lounge_menu} />
+      <Route exact path="/private-dining/les-salons-bernardin" component={les_salons_bernardin} />
+      <Route exact path="/private-dining/le-bernardin-prive" component={le_bernardin_prive} />
+      <Route exact path="/private-dining/inquiries" component={inquiries} />
+      <Route exact path="/menus/lounge/wine-and-spirits" component={wine_and_spirits} />
 
       {/*Reservations Pages*/}
+      <Route exact path="/menus/lounge/wine-and-spirits" component={wine_and_spirits} />
+      <Route exact path="/menus/lounge/wine-and-spirits" component={wine_and_spirits} />
+
       {/*Contact Us Pages*/}
       {/*Gifts Pages*/}
     
